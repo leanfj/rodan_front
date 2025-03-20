2d545255e9cc00bb5a2623e09712db9dd4f63050
Leandro Ferreira <leandro.ferreira@4mapit.com>
2025-03-19 23:54:01 -0300

    chore: commit inicial

---
+++ src/routes/__root.tsx
@@ -0,0 +1,43 @@
+import { QueryClient } from '@tanstack/react-query'
+import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { Toaster } from '@/components/ui/toaster'
+import GeneralError from '@/features/errors/general-error'
+import NotFoundError from '@/features/errors/not-found-error'
+
+interface AuthUser {
+  accountNo: string
+  email: string
+  role: string[]
+  exp: number
+}
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient
+  auth: {
+    user: AuthUser | null
+    setUser: (user: AuthUser | null) => void
+    accessToken: string
+    setAccessToken: (accessToken: string) => void
+    resetAccessToken: () => void
+    reset: () => void
+  }
+}>()({
+  component: () => {
+    return (
+      <>
+        <Outlet />
+        <Toaster />
+        {import.meta.env.MODE === 'development' && (
+          <>
+            <ReactQueryDevtools buttonPosition='bottom-left' />
+            <TanStackRouterDevtools position='bottom-right' />
+          </>
+        )}
+      </>
+    )
+  },
+  notFoundComponent: NotFoundError,
+  errorComponent: GeneralError,
+})
